af202605ed8829c39f5e8631645bb47f24f95d44
Noel Llevares <1972844+dashmug@users.noreply.github.com>
2024-02-24 12:03:47 +1100

    test: Improve test for custom conf

--- test/glue_utils/test_context.py
+++ test/glue_utils/test_context.py
@@ -1,4 +1,5 @@
 from awsglue.context import GlueContext
+from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
 from glue_utils.context import ManagedGlueContext
@@ -8,19 +9,27 @@ class TestManagedGlueContext:
     def test_init_without_options(self):
         context = ManagedGlueContext()
 
-        assert context.job_options == {}
+        assert context.options == {}
 
     def test_init_with_options(self):
         context = ManagedGlueContext(
-            job_options={"JOB_NAME": "test_job", "SOME_OPTION": "some_value"}
+            options={"JOB_NAME": "test_job", "SOME_OPTION": "some_value"}
         )
 
-        assert context.job_options == {
+        assert context.options == {
             "JOB_NAME": "test_job",
             "SOME_OPTION": "some_value",
         }
 
     def test_as_context_manager(self):
-        with ManagedGlueContext() as context:
+        options = {"JOB_NAME": "test_job"}
+        conf = SparkConf().set("Some.Dummy.Key", "Value")
+
+        with ManagedGlueContext(options=options, conf=conf) as context:
             assert isinstance(context, GlueContext)
             assert isinstance(context.spark_session, SparkSession)
+            assert context.spark_session.sparkContext.appName == "test_job"
+            assert (
+                context.spark_session.sparkContext.getConf().get("Some.Dummy.Key")
+                == "Value"
+            )
