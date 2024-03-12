43b02cd730b49f344150230ac71abc979a7488ad
Noel Martin Llevares <1972844+dashmug@users.noreply.github.com>
2024-03-12 20:03:23 +1100

    refactor: Restore to original code (#38)

--- jobs/sample/script.py
+++ jobs/sample/script.py
@@ -2,8 +2,7 @@
 This script is derived from the AWS Glue documentation example
 https://docs.aws.amazon.com/glue/latest/dg/aws-glue-programming-etl-libraries.html#develop-local-docker-image
 
-The code has been simplified to demonstrate how to use the
-ManagedGlueContext class from glue_utils.
+The code has been refactored to add static types and follow the linting rules.
 """
 
 import sys
@@ -12,10 +11,10 @@ from awsglue import DynamicFrame
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsglue.utils import getResolvedOptions
-from pyspark import SparkConf, SparkContext
+from pyspark import SparkContext
 
 
-def extract(glue_context: GlueContext, path: str) -> DynamicFrame:
+def read_json(glue_context: GlueContext, path: str) -> DynamicFrame:
     return glue_context.create_dynamic_frame_from_options(
         connection_type="s3",
         connection_options={"paths": [path], "recurse": True},
@@ -23,24 +22,28 @@ def extract(glue_context: GlueContext, path: str) -> DynamicFrame:
     )
 
 
-def run() -> None:
-    options = getResolvedOptions(sys.argv, [])
-    job_name = options.get("JOB_NAME", "sample_script_job")
+class GluePythonSample:
+    def __init__(self) -> None:
+        params = []
+        if "--JOB_NAME" in sys.argv:
+            params.append("JOB_NAME")
+        args = getResolvedOptions(sys.argv, params)
 
-    spark_conf = SparkConf().setAppName(job_name)
-    glue_context = GlueContext(SparkContext.getOrCreate(spark_conf))
+        self.context = GlueContext(SparkContext.getOrCreate())
+        self.job = Job(self.context)
 
-    job = Job(glue_context)
-    job.init(job_name, options)
+        job_name = args.get("JOB_NAME", "test")
+        self.job.init(job_name, args)
 
-    dynamicframe = extract(
-        glue_context=glue_context,
-        path="s3://awsglue-datasets/examples/us-legislators/all/persons.json",
-    )
-    dynamicframe.printSchema()
+    def run(self) -> None:
+        dynamicframe = read_json(
+            self.context,
+            "s3://awsglue-datasets/examples/us-legislators/all/persons.json",
+        )
+        dynamicframe.printSchema()
 
-    job.commit()
+        self.job.commit()
 
 
 if __name__ == "__main__":
-    run()
+    GluePythonSample().run()
