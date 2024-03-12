89c85f009732a8d90e83006589a7ec9a5a3ace11
Noel Llevares <1972844+dashmug@users.noreply.github.com>
2024-03-12 19:58:10 +1100

    refactor: Restore to original code

--- test/jobs/sample/test_script.py
+++ test/jobs/sample/test_script.py
@@ -4,13 +4,13 @@ from jobs.sample import script
 
 
 @patch("jobs.sample.script.GlueContext")
-def test_extract(mock_glue_context):
+def test_read_json(mock_glue_context):
     expected_count = 1961
     mock_glue_context.create_dynamic_frame_from_options.return_value.toDF.return_value.count.return_value = expected_count
 
     path = "s3://some-bucket-out-there/persons.json"
 
-    dyf = script.extract(glue_context=mock_glue_context, path=path)
+    dyf = script.read_json(glue_context=mock_glue_context, path=path)
 
     assert dyf.toDF().count() == expected_count
     mock_glue_context.create_dynamic_frame_from_options.assert_called_once_with(
@@ -22,7 +22,7 @@ def test_extract(mock_glue_context):
 
 @patch("jobs.sample.script.GlueContext")
 def test_run(mock_glue_context):
-    script.run()
+    script.GluePythonSample().run()
 
     mock_glue_context.return_value.create_dynamic_frame_from_options.assert_called_once_with(
         connection_type="s3",
